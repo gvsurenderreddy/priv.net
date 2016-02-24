c36e542d68113d1de1a87bd0faf387fc78ff4e9d
christos pollalis <christos pollalis>
2016-02-24 20:13:02 +0200

    minor changes

--- vpn_server.py
+++ vpn_server.py
@@ -7,25 +7,25 @@ import struct
 import fcntl
 from netaddr import *
 from pyroute2 import iproute
-from contextlib import contextmanager, closing
+from contextlib import contextmanager
 
 ipa = sys.argv[1]
 dev = sys.argv[2]
 
-private_networks = ['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16']
+settings = {}
+execfile("settings.py", settings)
+NETWORK_CONNECT_PRIVATE = settings.get('NETWORK_CONNECT_PRIVATE', '')  # 10.0.0.0/8, 172.16.0.0/12, 192.168.0.0/16
 
 
 # get local IP address of TUN
 def get_ip_address(iface):
 
-	from socket import socket, AF_INET, SOCK_STREAM, inet_ntoa
-
 	""" 0x8915 --> SIOCGIFADDR
         Get or set the address of the device using ifr_addr. Setting the interface address is a privileged operation.
         For compatibility, only AF_INET addresses are accepted or returned. """
 
-	sock = socket(AF_INET, SOCK_STREAM)
-	return inet_ntoa(fcntl.ioctl(sock.fileno(), 0x8915, struct.pack('256s', iface[:15]))[20:24])
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	return socket.inet_ntoa(fcntl.ioctl(sock.fileno(), 0x8915, struct.pack('256s', iface[:15]))[20:24])
 
 
 # check whether host belongs to a private network
@@ -34,7 +34,7 @@ def check_private_host(private_ip):
 	''' * call routing_conf inside
 		  check_private_host function instead ? '''
 
-	for cidr in private_networks:
+	for cidr in NETWORK_CONNECT_PRIVATE:
 		if IPAddress(str(private_ip)) in IPNetwork(str(cidr)):
 			return cidr
 		else:
@@ -57,54 +57,39 @@ def routing_conf():
 
 	handler = iproute.IPRoute()
 	index = handler.link_lookup(ifname=str(dev))[0]
-	VpnClient = (handler.get_addr(match=lambda x: x['index'] == int(index)))[0]['attrs'][0][1]
+	vpnclient = (handler.get_addr(match=lambda x: x['index'] == int(index)))[0]['attrs'][0][1]
 
 	add_ip_rule = subprocess.Popen('ip rule add from ' + str(get_ip_address(dev)) + ' table ' + str(table_id),
 	                               shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE).communicate()[0]
-	add_ip_route = subprocess.Popen('ip route add default scope global via ' + str(VpnClient) + ' dev ' + str(dev) +
+	add_ip_route = subprocess.Popen('ip route add default scope global via ' + str(vpnclient) + ' dev ' + str(dev) +
 	                                ' table ' + str(table_id), shell=True, stdout=subprocess.PIPE,
 	                                stdin=subprocess.PIPE).communicate()[0]
 	return
-
-routing_conf()
 
 
 # monkey patch socket to BINDTODEVICE
 @contextmanager
 def bind_device(dev):
 
-	import socket
 	_socket = socket.socket
 
 	class Socket(_socket):
 		def __init__(self, *args, **kwargs):
-			super(Socket, self).__init__()
+			super(Socket, self).__init__(*args, **kwargs)
 			self.setsockopt(socket.SOL_SOCKET, 25, dev)  # define SO_BINDTODEVICE 25
-
-		'''
-		def __enter__(self):
-			socket.socket = Socket
-			return
-
-		def __exit__(self, exc_type, exc_val, exc_tb):
-			self.close()
-		'''
 
 		def unbind(self, *args, **kwargs):
 			self.close()
 
 	try:
 		socket.socket = Socket
-		# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		yield socket.socket  # s
+		yield socket.socket
 	finally:
 		if not socket._closedsocket:
 			Socket.unbind()
-		# s.close()
-		# e = socket.socket
-		# e.unbind()
+		socket.socket = _socket
 
-with bind_device(dev):  # as s:
+with bind_device(dev):
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	print('Bind DONE: ' + str(dev))
